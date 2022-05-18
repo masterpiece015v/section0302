fdf12b926aca8da929ab9f44261ec969ee584238
masterpiece015v <masterpiece.015v@gmail.com>
2022-05-18 10:47:46 +0900

    0518

--- app/src/main/java/com/example/section0302/MainActivity.kt
+++ app/src/main/java/com/example/section0302/MainActivity.kt
@@ -2,8 +2,8 @@ package com.example.section0302
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.CheckBox
-import android.widget.Toast
+import android.widget.*
+import java.util.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,12 +14,48 @@ class MainActivity : AppCompatActivity() {
         chk.setOnCheckedChangeListener{buttonView,isChecked->
 
             Toast.makeText(
-                this@MainActivity,
+                this@MainActivity ,
                 if(isChecked)"メール送信をオン" else "メール送信をオフ",
                 Toast.LENGTH_SHORT
             ).show()
 
 
         }
+
+        val toggle = findViewById<ToggleButton>(R.id.toggle)
+        toggle.setOnCheckedChangeListener{view,isChecked->
+            val t = view as ToggleButton
+            Toast.makeText(
+                this,
+                if(isChecked) t.textOn else t.textOff ,
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        val rgroup = findViewById<RadioGroup>(R.id.rgroup)
+        rgroup.setOnCheckedChangeListener{group,checkedId->
+            val radio = group.findViewById<RadioButton>(checkedId)
+            Toast.makeText(
+                this,
+                String.format("「%s」が選択されました。",radio.text),
+                Toast.LENGTH_SHORT
+            ).show()
+        }
+
+        val seek = findViewById<SeekBar>(R.id.seek)
+        seek.setOnSeekBarChangeListener(object:SeekBar.OnSeekBarChangeListener{
+            override fun onProgressChanged(p0: SeekBar?, p1: Int, p2: Boolean) {
+
+                Toast.makeText(
+                    this@MainActivity,
+                    String.format(Locale.JAPAN,"現在地:%d",p1),
+                    Toast.LENGTH_SHORT
+                ).show()
+
+            }
+
+            override fun onStartTrackingTouch(p0: SeekBar?) {}
+            override fun onStopTrackingTouch(p0: SeekBar?) {}
+        })
     }
 }
